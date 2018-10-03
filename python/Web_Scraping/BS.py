82a801778483ec332afebcaf6d130242a58a10b8
jairo melo <jairom@colmich.edu.mx>
2018-10-03 12:06:24 -0500

    crea archivo en lugar de print()

--- python/Web_Scraping/BS.py
+++ python/Web_Scraping/BS.py
@@ -1,15 +1,24 @@
 from bs4 import BeautifulSoup
 import urllib.request
+# importa el archivo lista.py, que contiene los números de los elementos ("items") dentro del repositorio
 import lista
 
+# guarda en la función "url_base" la dirección "estática" de cada elemento
 url_base = "http://localhost/fichero/items/show/{}"
 repeticion = lista.mi_lista
 
-for i in range(0,681):
-	sauce = urllib.request.urlopen(url_base.format(repeticion[i])).read()
-	soup = BeautifulSoup(sauce, "lxml")
-	for div in soup.find_all('div', class_='element-text'):
+# crea el archivo "texto_elementos.txt" donde se escribirá la información. Puede modificar el nombre del archivo por cualquier otro.
+# modifique el nombre cada vez que haga una modificación, de otra manera el archivo de sobreescribe sin advertencia.
+f = open("texto_elementos.txt", "w+")
+
+# loop para llamar cada página del repositorio y recolectar solo la información de los div que contengan la clase 'element-text'
+# modifique los parámetros de la función range() para que se ajusten a la cantidad de elementos que contenga el repositorio (total -1) 
+for i in range(0,3):
+	salsa = urllib.request.urlopen(url_base.format(repeticion[i])).read()
+	sopa = BeautifulSoup(salsa, "lxml")
+	for div in sopa.find_all('div', class_='element-text'):
 		try:
-			print(div.text)
+			f.write(div.text)
+		# En caso de error, el programa seguirá corriendo y recuperará los elementos que pueda escrcibir. (No estoy muy seguro que funcione, así que, recibo sugerencias :p)
 		except:
 			pass
