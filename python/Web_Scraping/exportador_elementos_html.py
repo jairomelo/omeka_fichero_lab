f526c2f9f05763e7862857d3724f76421f8117a0
jairomelo <jairom@colmich.edu.mx>
2018-12-15 00:13:58 -0600

    advertencias módulos, codificación utf-8 y conversión a docx

--- python/Web_Scraping/exportador_elementos_html.py
+++ python/Web_Scraping/exportador_elementos_html.py
@@ -1,22 +1,37 @@
-from bs4 import BeautifulSoup
+import re, sys, time, codecs
 import urllib.request
-import re, os
+
+#Módulos que necesitan ser instalados
+try:
+	from bs4 import BeautifulSoup
+except ImportError:
+	print("antes de usar el programa instale BeautifulSoup `pip install beautifulsoup4`")
+	time.sleep(5)
+	sys.exit()
+
+try:
+	import pypandoc
+except ImportError:
+	print("antes de usar el programa instale pypandoc `pip install pypandoc`")
+	time.sleep(5)
+	sys.exit()
 
 busca = input('pegar la búsqueda: ')
 archivo = input('nombre del archivo de resultados: ')
-
+archivo_html = "{}.html".format(archivo)
 host = "http://localhost"
 
 salsa = urllib.request.urlopen(busca).read()
-sopa = BeautifulSoup(salsa, 'html.parser')
+sopa = BeautifulSoup(salsa, "lxml")
 
 rutas = []
-
 for links in sopa.find_all('a', class_='permalink'):
 		obtener = "{}{}".format(host, links["href"])
 		rutas.append(obtener)
 
-f = open("{}.html".format(archivo), "w+")
+f = codecs.open(archivo_html, "w+", "utf-8")
+
+print("creando archivo html con los resultados")
 
 for i in range(len(rutas)):
 	cadenas = str(rutas)
@@ -27,3 +42,17 @@ for i in range(len(rutas)):
 	sopa = BeautifulSoup(salsa, 'html.parser')
 	for div in sopa.find_all('div', class_='element-text'):
 			f.write(div.prettify())
+f.close()
+
+time.sleep(2)
+
+convertir_docx = input('¿Desea convertir el archivo HTML a Word? S/N: ')
+
+if convertir_docx in "S" or "s":
+	archivo_docx = "{}.docx".format(archivo)
+	documento = pypandoc.convert_file(archivo_html, "docx", outputfile=archivo_docx)
+	assert documento == ""
+elif convertir_docx in "N" or "n":
+	sys.exit()
+else:
+	sys.exit()
